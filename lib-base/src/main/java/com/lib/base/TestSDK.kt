02ee4b67f04c47f31aaa859ed4c54f885d5a2083
sziitjin <linguangjin@toro-tech.com>
2024-06-06 17:47:13 +0800

    init

---
+++ lib-base/src/main/java/com/lib/base/TestSDK.kt
@@ -0,0 +1,15 @@
+/*年轻人，只管向前看，不要管自暴自弃者的话*/
+package com.lib.base
+
+
+/**
+ * create by itz on 2024/6/6 17:41
+ * desc :
+ **/
+class TestSDK {
+    companion object{
+        fun outData(){
+            print("holle sdk")
+        }
+    }
+}
